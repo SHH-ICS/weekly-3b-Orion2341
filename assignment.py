99c0248a36fae6ff2e1f97623cef7321ccad8231
Orion2341 <144241864+Orion2341@users.noreply.github.com>
2023-11-02 12:58:28 +0000

    added code

--- assignment.py
+++ assignment.py
@@ -1,1 +1,9 @@
-
+import math
+txt = "Welcome to Juicy Pizzas!"
+txt2 = "Choose your size! Large, or Extra Large!"
+size = input("Enter size")
+a = txt.center(20)
+b = txt.center(20)
+print(a)
+print(txt2)
+print("Great you chose" + size )
