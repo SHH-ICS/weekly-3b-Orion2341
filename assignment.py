f0cee52543a701da5d19c5f62ff9346343ab3cb6
Orion2341 <144241864+Orion2341@users.noreply.github.com>
2023-11-02 13:13:38 +0000

    added code

--- assignment.py
+++ assignment.py
@@ -1,9 +1,10 @@
 import math
-txt = "Welcome to Juicy Pizzas!"
-txt2 = "Choose your size! Large, or Extra Large!"
-size = input("Enter size")
-a = txt.center(20)
-b = txt2.center(20)
-print(a)
-print(b)
-print("Great you chose" + size )
+totalCost = 0
+def myTexts():
+    txt = "Welcome to Juicy Pizzas!"
+    txt2 = "Choose your size! Large, or Extra Large!"
+    a = txt.center(20)
+    b = txt2.center(20)
+    print(a)
+    print(b)
+myTexts()
