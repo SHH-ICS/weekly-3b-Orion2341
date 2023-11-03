51efbccb737af00d2b5be46e6c2b8ae293a6d308
Orion2341 <144241864+Orion2341@users.noreply.github.com>
2023-11-03 12:24:50 +0000

    added code

--- assignment.py
+++ assignment.py
@@ -2,6 +2,10 @@ import math
 totalCost = 0
 Large = 6
 ExtraLarge = 10
+onetopping = 1
+twotoppings = 1.75
+threetoppings = 2.50
+fourtoppings = 3.35
 def myTexts():
     txt = "Welcome to Juicy Pizzas!"
     txt2 = "Choose your size! Large, or Extra Large!"
