c3d52420f5268bf3f283b880dcf155a74de7eaf5
Orion2341 <144241864+Orion2341@users.noreply.github.com>
2023-11-07 22:58:49 +0000

    added code

--- assignment.py
+++ assignment.py
@@ -1,16 +1,44 @@
 import math
-pizzaCost = 0
-subTotal = 0
-tax = 0
-totalCost = 0
+pizzaSizeCost = 0
+pizzaToppingsCost = {
+    1: 1,
+    2: 1.75,
+    3: 2.5,
+    4: 3.35
+}
+taxRate = 0.13
 Large = 6
 ExtraLarge = 10
 print("Welcome to Juicy Pizzas!")
-print("Choose your size! 'Large', or 'Extra Large'!")
+print("Choose your size! 'Large', or 'ExtraLarge'!")
 pizzaSize = input()
 if pizzaSize == "Large":
-    pizzaCost = Large
+    pizzaSizeCost = Large
 elif pizzaSize == "ExtraLarge":
-    pizzaCost = ExtraLarge
+    pizzaSizeCost = ExtraLarge
+else:
+    print("Invalid size of pizza")
+    print("Try again!")
+    exit()
 print("Great! now it's time to choose your toppings")
-print("How many toppings would you like between 1 and 4?")
+print("How many toppings would you like to choose?")
+print("one, two, three, or four")
+numberOfToppings = input()
+if numberOfToppings == "one":
+    pizzaToppingsCost + one
+elif numberOfToppings == "two":
+    pizzaToppingsCost + two
+elif numberOfToppings == "three":
+    pizzaToppingsCost + three
+elif numberOfToppings == "four":
+    pizzaToppingsCost + four
+else:
+    print("Invalid number of toppings")
+    print("Try again!")
+    exit()
+subTotal = pizzaSizeCost + pizzaToppingsCost
+tax = subTotal * taxRate
+totalCost = subTotal + taxRate
+print(subTotal)
+print(tax)
+print(totalCost)
